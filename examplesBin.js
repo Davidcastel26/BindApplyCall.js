80768faccead3981fcb3bc3ec2552520942ea3f1
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-27 18:46:35 -0600

    Create examplesBin.js

---
+++ examplesBin.js
@@ -0,0 +1,20 @@
+// bin examples 
+
+let coach = {
+    name:"Frak",
+    age: 25
+  }
+  
+  let aluminium = {
+    name: "Jhon",
+    course: "FullStack"
+  }
+  
+  function getName(){
+    return this.name;
+  }
+  
+  getNameCoach = getName.bind(coach)
+  // getNameCoach = function (){ return coach.name}
+  getNameAluminium = getName.bind(aluminium)
+  console.log(getNameAluminium()) // 'Jhon'
